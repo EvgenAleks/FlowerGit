c48aaa665191b92f9b6178a5e05cd8fd4b1a3419
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-04-15 18:19:08 +0200

    15046

--- src/Flowers.java
+++ src/Flowers.java
@@ -4,10 +4,12 @@ public class Flowers {
 
     public String name;
     public String color;
+    public String countryOfOrigin;
 
-    Flowers(String flName, String flColor) {
+    Flowers(String flName, String flColor, String country) {
         name = flName;
         color = flColor;
+        countryOfOrigin = country;
     }
 
 
@@ -15,11 +17,12 @@ public class Flowers {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please enter name and color of the flower");
+        System.out.println("Please enter flower's name, color and country of origin");
 
-        Flowers flower = new Flowers(scan.next(), scan.next());
+        Flowers flower = new Flowers(scan.next(), scan.next(), scan.next());
 
         System.out.println("The " + flower.name + " is " + flower.color);
+        System.out.println("The " + flower.color + " " + flower.name + " comes from " + flower.countryOfOrigin);
     }
 
 }
